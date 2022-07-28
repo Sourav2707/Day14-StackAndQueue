ba880f0d2eae2de2817675229dbaacfe53f102a0
Sourav2707 <smartsourav33@gmail.com>
2022-07-28 14:29:35 +0530

    creating a stack

--- StackAndQueue/StackAndQueue/Program.cs
+++ StackAndQueue/StackAndQueue/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Welcome to Stack and queue programs");
+﻿using StackAndQueue;
+
+Console.WriteLine("Welcome to Stack and queue programs");
+LinkedListStack linkedListStack = new LinkedListStack();
+linkedListStack.Push(70);
+linkedListStack.Push(30);
+linkedListStack.Push(56);
+linkedListStack.Display();
